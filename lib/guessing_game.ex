93278fc8d51800f02e12df58fe3ca405c6036991
gandhiShepard <benjamin@gandhishepard.com>
2024-02-22 10:58:17 -0600

    update tests and add previous guesses

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -34,7 +34,6 @@ defmodule Games.GuessingGame do
     play_again()
   end
 
-
   def play(%__MODULE__{guesses: guesses}) when length(guesses) == 5 do
    IO.puts("#{IO.ANSI.red()} Sorry, you lost! You ran out of guesses!")
 
@@ -44,6 +43,7 @@ defmodule Games.GuessingGame do
   def play(%__MODULE__{winning_number: n, guesses: [g | _t] = gs} = game) when g > n do
     IO.puts("""
     #{IO.ANSI.magenta()} Guess is greater than winning number.
+    Here are your previous guesses #{Enum.join(gs, ", ")}.
     #{guesses_left(gs)}
     """)
 
@@ -52,9 +52,9 @@ defmodule Games.GuessingGame do
   end
 
   def play(%__MODULE__{winning_number: n, guesses: [g | _t] = gs} = game) when g < n do
-
     IO.puts("""
     #{IO.ANSI.magenta()} Guess is less than winning number.
+    Here are your previous guesses: [#{Enum.join(gs, ", ")}].
     #{guesses_left(gs)}
     """)
 
@@ -62,44 +62,34 @@ defmodule Games.GuessingGame do
     play(game)
   end
 
-  def play_again do
-    play_again = IO.gets("#{IO.ANSI.blue()}Do you want to play again? Enter Y for yes or N for no. ")
-    case String.trim(String.upcase(play_again)) do
-      "Y" -> play()
-      "N" -> "Ok, later!"
-      _ -> "Input not accepted"
-    end
-  end
-
-
-  # @spec guess :: 1..10
-  # def guess do
-  #   IO.gets("#{IO.ANSI.blue()} Guess a number between 1 and 10: ")
-  #   |> String.trim()
-  #   |> String.to_integer()
-  # end
-
+  @spec guess :: integer
   def guess do
-    guess(IO.gets("#{IO.ANSI.blue()} Guess a number between 1 and 10: "), ~r/([0-9]{1,2})/, "Guess must be a number between 1 and 10: ")
-  end
-
-  def guess(name_input, accepted_name, failure) do
-    if String.match?(name_input, accepted_name) do
-      name_input
-      |> String.trim()
-      |> String.to_integer()
-    else
-      guess(IO.gets(failure), accepted_name, failure)
+    name_input = IO.gets("#{IO.ANSI.blue()} Guess a number between 1 and 10: ")
+    case String.match?(name_input, ~r/([0-9]{1,2})/) do
+      true ->
+        name_input
+        |> String.trim()
+        |> String.to_integer()
+      false ->
+        IO.puts("That's not a number fool!\n")
+        guess()
     end
   end
-
 
   defp guesses_left(list) do
     guesses_left = length(list)
     "You have #{5 - guesses_left} #{if guesses_left == 1, do: "guess", else: "guesses"} left, try again."
   end
 
-  @spec random_number :: 1..10
+  defp play_again do
+    play_again = IO.gets("#{IO.ANSI.blue()}Do you want to play again? Enter Y for yes or N for no. ")
+    case String.trim(String.upcase(play_again)) do
+      "Y" -> play()
+      "N" -> "Ok, later!"
+       _ -> "Input not accepted"
+    end
+  end
+
   defp random_number do
     Enum.random(1..10)
   end
