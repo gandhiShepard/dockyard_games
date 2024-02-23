33a2b1f0c56efabbd3411d4b4235deca8e1222f9
gandhiShepard <benjamin@gandhishepard.com>
2024-02-23 13:41:37 -0600

    add .iex and colors to prompt

---
+++ .iex.exs
@@ -0,0 +1,70 @@
+# aliases for module usage
+# import Ecto.Query
+
+alias Games.{GuessingGame, RockPaperScissors}
+
+# Custom IEX
+# https://github.com/blackode/elixir-tips/blob/master/part1.md
+# https://www.adiiyengar.com/blog/20180709/my-iex-exs
+# https://samuelmullen.com/articles/customizing_elixirs_iex/
+# https://samuelmullen.com/articles/customizing_elixirs_iex
+# https://alchemist.camp/episodes/iex-exs
+#
+
+# :light_black is dark grey, :white is light grey, and :light_white is actually white.
+
+Application.put_env(:elixir, :ansi_enabled, true)
+
+counter = IO.ANSI.green() <> "%counter" <> IO.ANSI.reset()
+yay     = IO.ANSI.green() <> "(ノ＾ω＾)ノ" <> IO.ANSI.red() <> "❤ " <> IO.ANSI.reset()
+emo     = IO.ANSI.green() <> "(///_^)" <> IO.ANSI.reset()
+oops    = "(╯°□°）╯"
+kiss    = "( ˘ ³˘)~"
+cat     = "/ᐠ。ꞈ。ᐟ\~"
+monster = "|°▿▿▿▿°|"
+happy   = "ツ"
+
+alive   = IO.ANSI.bright() <> IO.ANSI.yellow() <> IO.ANSI.blink_rapid() <> "⚡" <> IO.ANSI.reset()
+
+default_prompt = yay <> counter
+alive_prompt   = emo <> counter <> alive
+
+inspect_limit = 5_000
+history_size  = 100
+
+eval_result = [:green, :bright]
+eval_error  = [[:red, :bright], oops]
+eval_info   = [:blue, :bright]
+
+IEx.configure [
+    inspect: [
+    limit: inspect_limit,
+    charlists: :as_lists,
+    pretty: true,
+    binaries: :as_strings,
+    printable_limit: :infinity
+  ],
+  history_size: history_size,
+  colors: [
+    syntax_colors: [
+      number: :light_yellow,
+      atom: :light_cyan,
+      string: :light_black,
+      boolean: :red,
+      nil: [:magenta, :bright],
+    ],
+
+    eval_result: eval_result,
+    eval_error: eval_error,
+    eval_info: eval_info,
+
+    ls_directory: :cyan,
+    ls_device: :yellow,
+    doc_code: :green,
+    doc_inline_code: :magenta,
+    doc_headings: [:cyan, :underline],
+    doc_title: [:cyan, :bright, :underline],
+  ],
+  default_prompt: default_prompt,
+  alive_prompt: alive_prompt,
+]
--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -1,6 +1,7 @@
 defmodule Games.RockPaperScissors do
   @type t :: %__MODULE__{
-
+    ai_choice: String.t,
+    users_choice: String.t
   }
 
   @enforce_keys [:ai_choice, :users_choice]
@@ -14,38 +15,42 @@ defmodule Games.RockPaperScissors do
     }
   end
 
+  # Not sure what to put as the typespec
+  # because the return value is IO.
+  # How do I account for this?
   def play(game \\ new()) do
     game = %{game | users_choice: choose()}
     IO.puts(game.ai_choice <> " <> " <> game.users_choice)
     win_or_lose(game.ai_choice, game.users_choice)
   end
 
+  @spec choose :: String.t
   def choose do
     users_choice =
-      IO.gets("Choose R for rock, P for paper or S for scissors: ")
+      IO.gets("Choose #{IO.ANSI.cyan()}R#{IO.ANSI.reset()} for rock, #{IO.ANSI.cyan()}P#{IO.ANSI.reset()} for paper or #{IO.ANSI.cyan()}S#{IO.ANSI.reset()} for scissors: ")
       |> String.upcase()
       |> String.trim()
 
     case users_choice in ~w[R P S] do
       true -> initial_to_choice(users_choice)
-      false -> IO.puts("Cant to that fucker")
+      false -> IO.puts("Can't do that fucker!")
     end
   end
 
   defp win_or_lose("ROCK", "SCISSORS"), do:
-    IO.puts("You lose. AI chose ROCK and you chose SCISSORS")
+    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}ROCK#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}SCISSORS#{IO.ANSI.reset()}")
 
   defp win_or_lose("PAPER", "ROCK"), do:
-    IO.puts("You lose. AI chose PAPER and you chose ROCK")
+    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}PAPER#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}ROCK#{IO.ANSI.reset()} ")
 
   defp win_or_lose("SCISSORS", "PAPER"), do:
-    IO.puts("You lose. AI chose SCISSORS and you chose PAPER")
+    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}SCISSORS#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}PAPER#{IO.ANSI.reset()} ")
 
   defp win_or_lose(ai_choice, ai_choice), do:
-    IO.puts("Draw. You both chose #{ai_choice}")
+    IO.puts("#{IO.ANSI.yellow_background()}Draw.#{IO.ANSI.reset()} You both chose #{IO.ANSI.yellow_background()}#{ai_choice}#{IO.ANSI.reset()}")
 
   defp win_or_lose(ai_choice, users_choice), do:
-    IO.puts("You WIN! AI chose #{ai_choice} and you chose #{users_choice}")
+    IO.puts("#{IO.ANSI.green()}You WIN!#{IO.ANSI.reset()} AI chose #{IO.ANSI.red()}#{ai_choice}#{IO.ANSI.reset()} and you chose #{IO.ANSI.green_background()}#{users_choice}#{IO.ANSI.reset()}")
 
   defp initial_to_choice("R"), do: "ROCK"
   defp initial_to_choice("P"), do: "PAPER"
