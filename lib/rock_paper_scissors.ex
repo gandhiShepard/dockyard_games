eb20dce66f81f459e588498ff6758f022212021a
gandhiShepard <benjamin@gandhishepard.com>
2024-02-28 05:44:38 -0600

    refactor RPS and wordle to handle_guess

--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -19,41 +19,46 @@ defmodule Games.RockPaperScissors do
   # because the return value is IO.
   # How do I account for this?
   def play(game \\ new()) do
-    game = %{game | users_choice: choose()}
-    IO.puts(game.ai_choice <> " <> " <> game.users_choice)
-    win_or_lose(game.ai_choice, game.users_choice)
-  end
+    # choice =
+    # game = %{game | users_choice: choose()}
+    # IO.puts(game.ai_choice <> " <> " <> game.users_choice)
+    # win_or_lose(game.ai_choice, game.users_choice)
 
-  @spec choose :: String.t
-  def choose do
     users_choice =
       IO.gets("Choose #{IO.ANSI.cyan()}R#{IO.ANSI.reset()} for rock, #{IO.ANSI.cyan()}P#{IO.ANSI.reset()} for paper or #{IO.ANSI.cyan()}S#{IO.ANSI.reset()} for scissors: ")
       |> String.upcase()
       |> String.trim()
 
-    case users_choice in ~w[R P S] do
-      true -> initial_to_choice(users_choice)
-      false -> IO.puts("Can't do that fucker!")
+    case users_choice do
+      "R" -> handle_choice(game, {:ok, "ROCK"})
+      "P" -> handle_choice(game, {:ok, "PAPER"})
+      "S" -> handle_choice(game, {:ok, "SCISSORS"})
+      _ -> handle_choice(game, {:error, "invalid input"})
     end
   end
 
-  defp win_or_lose("ROCK", "SCISSORS"), do:
-    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}ROCK#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}SCISSORS#{IO.ANSI.reset()}")
-
-  defp win_or_lose("PAPER", "ROCK"), do:
-    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}PAPER#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}ROCK#{IO.ANSI.reset()} ")
-
-  defp win_or_lose("SCISSORS", "PAPER"), do:
-    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}SCISSORS#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}PAPER#{IO.ANSI.reset()} ")
-
-  defp win_or_lose(ai_choice, ai_choice), do:
+  defp handle_choice(%__MODULE__{ai_choice: ai_choice}, {:ok, ai_choice}) do
     IO.puts("#{IO.ANSI.yellow_background()}Draw.#{IO.ANSI.reset()} You both chose #{IO.ANSI.yellow_background()}#{ai_choice}#{IO.ANSI.reset()}")
+  end
 
-  defp win_or_lose(ai_choice, users_choice), do:
+  defp handle_choice(%__MODULE__{ai_choice: "ROCK"}, {:ok, "SCISSORS"}) do
+    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}ROCK#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}SCISSORS#{IO.ANSI.reset()}")
+  end
+
+  defp handle_choice(%__MODULE__{ai_choice: "PAPER"}, {:ok, "ROCK"}) do
+    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}PAPER#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}ROCK#{IO.ANSI.reset()} ")
+  end
+
+  defp handle_choice(%__MODULE__{ai_choice: "SCISSORS"}, {:ok, "PAPER"}) do
+    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}SCISSORS#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}PAPER#{IO.ANSI.reset()} ")
+  end
+
+  defp handle_choice(%__MODULE__{ai_choice: ai_choice}, {:ok, users_choice}) do
     IO.puts("#{IO.ANSI.green()}You WIN!#{IO.ANSI.reset()} AI chose #{IO.ANSI.red()}#{ai_choice}#{IO.ANSI.reset()} and you chose #{IO.ANSI.green_background()}#{users_choice}#{IO.ANSI.reset()}")
+  end
 
-  defp initial_to_choice("R"), do: "ROCK"
-  defp initial_to_choice("P"), do: "PAPER"
-  defp initial_to_choice("S"), do: "SCISSORS"
-
+  defp handle_choice(game, {:error, _msg}) do
+    IO.puts("Can't do that fucker! Try again.")
+    play(game)
+  end
 end
--- test/rock_paper_scissors_test.exs
+++ test/rock_paper_scissors_test.exs
@@ -8,21 +8,21 @@ defmodule RockPaperScissorsTest do
     assert %RockPaperScissors{ai_choice: _, users_choice: nil} = RockPaperScissors.new()
   end
 
-  test "user can choose R" do
-    assert capture_io([input: "R", capture_prompt: false], fn ->
-      IO.write RockPaperScissors.choose()
-    end) == "ROCK"
-  end
+  # test "user can choose R" do
+  #   assert capture_io([input: "R", capture_prompt: false], fn ->
+  #     IO.write RockPaperScissors.choose()
+  #   end) == "ROCK"
+  # end
 
-  test "user can choose P" do
-    assert capture_io([input: "P", capture_prompt: false], fn ->
-      IO.write RockPaperScissors.choose()
-    end) == "PAPER"
-  end
+  # test "user can choose P" do
+  #   assert capture_io([input: "P", capture_prompt: false], fn ->
+  #     IO.write RockPaperScissors.choose()
+  #   end) == "PAPER"
+  # end
 
-  test "user can choose S" do
-    assert capture_io([input: "S", capture_prompt: false], fn ->
-      IO.write RockPaperScissors.choose()
-    end) == "SCISSORS"
-  end
+  # test "user can choose S" do
+  #   assert capture_io([input: "S", capture_prompt: false], fn ->
+  #     IO.write RockPaperScissors.choose()
+  #   end) == "SCISSORS"
+  # end
 end
