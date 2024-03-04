8e52851c1afadfa856f73119d749851e9fbabb2e
gandhiShepard <benjamin@gandhishepard.com>
2024-03-04 12:00:57 -0600

    refactor GuessingGame and RockPaperScissors

--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -1,64 +1,121 @@
 defmodule Games.RockPaperScissors do
   @type t :: %__MODULE__{
-    ai_choice: String.t,
-    users_choice: String.t
+    ai_choice: choice,
+    users_choice: choice,
+    game_status: status
   }
 
-  @enforce_keys [:ai_choice, :users_choice]
-  defstruct [:ai_choice, :users_choice]
+  @type game :: t
+  @type status ::  nil | :win | :lose | :draw | :invalid_choice
+  @type choice :: rock | paper | scissors
+  @type rock :: String.t
+  @type paper :: String.t
+  @type scissors :: String.t
+  @type invalid :: String.t
 
-  @spec new :: RockPaperScissors.t
+  @enforce_keys [:ai_choice, :users_choice, :game_status]
+  defstruct [:ai_choice, :users_choice, :game_status]
+
+  @spec new :: game
   def new do
     %__MODULE__{
       ai_choice: Enum.random(["ROCK", "PAPER", "SCISSORS"]),
-      users_choice: nil
+      users_choice: nil,
+      game_status: nil
     }
   end
 
-  # Not sure what to put as the typespec
-  # because the return value is IO.
-  # How do I account for this?
-  def play(game \\ new()) do
-    # choice =
-    # game = %{game | users_choice: choose()}
-    # IO.puts(game.ai_choice <> " <> " <> game.users_choice)
-    # win_or_lose(game.ai_choice, game.users_choice)
+  def play(game \\ new())
 
-    users_choice =
-      IO.gets("Choose #{IO.ANSI.cyan()}R#{IO.ANSI.reset()} for rock, #{IO.ANSI.cyan()}P#{IO.ANSI.reset()} for paper or #{IO.ANSI.cyan()}S#{IO.ANSI.reset()} for scissors: ")
-      |> String.upcase()
-      |> String.trim()
-
-    case users_choice do
-      "R" -> handle_choice(game, {:ok, "ROCK"})
-      "P" -> handle_choice(game, {:ok, "PAPER"})
-      "S" -> handle_choice(game, {:ok, "SCISSORS"})
-      _ -> handle_choice(game, {:error, "invalid input"})
-    end
+  def play(game) when game.game_status in [nil, :invalid_choice] do
+    choice_prompt()
+    |> enter_users_choice(game)
+    |> evaluate_choices()
+    |> print_result()
+    |> play()
   end
 
-  defp handle_choice(%__MODULE__{ai_choice: ai_choice}, {:ok, ai_choice}) do
-    IO.puts("#{IO.ANSI.yellow_background()}Draw.#{IO.ANSI.reset()} You both chose #{IO.ANSI.yellow_background()}#{ai_choice}#{IO.ANSI.reset()}")
+  def play(_game), do: Games.main()
+
+  @spec choice_prompt :: String.t
+  def choice_prompt do
+    IO.gets("""
+      Choose:
+      #{IO.ANSI.cyan()}R#{IO.ANSI.reset()} for rock
+      #{IO.ANSI.cyan()}P#{IO.ANSI.reset()} for paper
+      or #{IO.ANSI.cyan()}S#{IO.ANSI.reset()} for scissors:
+      """)
   end
 
-  defp handle_choice(%__MODULE__{ai_choice: "ROCK"}, {:ok, "SCISSORS"}) do
-    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}ROCK#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}SCISSORS#{IO.ANSI.reset()}")
-  end
+  @spec enter_users_choice(choice, game) :: game
+  def enter_users_choice(choice, game), do:
+    %{game | users_choice: format_choice(choice)}
 
-  defp handle_choice(%__MODULE__{ai_choice: "PAPER"}, {:ok, "ROCK"}) do
-    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}PAPER#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}ROCK#{IO.ANSI.reset()} ")
-  end
+  @spec evaluate_choices(game) :: game
+  def evaluate_choices(%__MODULE__{users_choice: "invalid"} = game), do:
+    %{game | game_status: :invalid_choice}
 
-  defp handle_choice(%__MODULE__{ai_choice: "SCISSORS"}, {:ok, "PAPER"}) do
-    IO.puts("#{IO.ANSI.red()}You lose.#{IO.ANSI.reset()} AI chose #{IO.ANSI.green_background()}SCISSORS#{IO.ANSI.reset()} and you chose #{IO.ANSI.red()}PAPER#{IO.ANSI.reset()} ")
-  end
+  def evaluate_choices(%__MODULE__{ai_choice: "ROCK", users_choice: "SCISSORS"} = game), do:
+    %{game | game_status: :lose}
 
-  defp handle_choice(%__MODULE__{ai_choice: ai_choice}, {:ok, users_choice}) do
-    IO.puts("#{IO.ANSI.green()}You WIN!#{IO.ANSI.reset()} AI chose #{IO.ANSI.red()}#{ai_choice}#{IO.ANSI.reset()} and you chose #{IO.ANSI.green_background()}#{users_choice}#{IO.ANSI.reset()}")
-  end
+  def evaluate_choices(%__MODULE__{ai_choice: "PAPER", users_choice: "ROCK"} = game), do:
+    %{game | game_status: :lose}
 
-  defp handle_choice(game, {:error, _msg}) do
+  def evaluate_choices(%__module__{ai_choice: "SCISSORS", users_choice: "PAPER"} = game), do:
+    %{game | game_status: :lose}
+
+  def evaluate_choices(%__module__{ai_choice: same, users_choice: same} = game), do:
+    %{game | game_status: :draw}
+
+  def evaluate_choices(%__MODULE__{} = game), do:
+    %{game | game_status: :win}
+
+  @spec print_result(game) :: game
+  def print_result(%__MODULE__{game_status: :invalid_choice} = game) do
     IO.puts("Can't do that fucker! Try again.")
-    play(game)
+    game
   end
+
+  def print_result(%__MODULE__{game_status: :draw} = game) do
+    IO.puts("""
+      #{IO.ANSI.yellow()}
+      DRAW! You both chose #{game.ai_choice}.
+      #{IO.ANSI.reset()}
+      """)
+    game
+  end
+
+  def print_result(%__MODULE__{game_status: :lose} = game) do
+  IO.puts("""
+    #{IO.ANSI.red()}
+    You LOSE!
+    AI chose #{game.ai_choice}.
+    You chose #{game.users_choice}.
+    #{IO.ANSI.reset()}
+    """)
+    game
+  end
+
+  def print_result(%__MODULE__{game_status: :win} = game) do
+    IO.puts("""
+      #{IO.ANSI.green()}You WIN!
+      AI chose #{game.ai_choice}.
+      You chose #{game.users_choice}.
+      #{IO.ANSI.reset()}
+      """)
+    game
+  end
+
+  defp format_choice(str) do
+      str
+      |> String.trim()
+      |> String.upcase()
+      |> then(fn
+          "R" -> "ROCK"
+          "P" -> "PAPER"
+          "S" -> "SCISSORS"
+          _invalid -> "invalid"
+        end)
+    end
+
 end
--- test/rock_paper_scissors_test.exs
+++ test/rock_paper_scissors_test.exs
@@ -1,28 +1,162 @@
 defmodule RockPaperScissorsTest do
   use ExUnit.Case
-  # import ExUnit.CaptureIO
+  import ExUnit.CaptureIO
 
   alias Games.RockPaperScissors
 
-  test "new create a new RPS game" do
-    assert %RockPaperScissors{ai_choice: _, users_choice: nil} = RockPaperScissors.new()
+  describe "new/0" do
+    test "creates a game struct" do
+      assert %RockPaperScissors{} = RockPaperScissors.new()
+    end
+
+    test "initiates the ai_choice with a random choice" do
+      assert %RockPaperScissors{ai_choice: choice} = RockPaperScissors.new()
+      assert choice in ~w[ROCK PAPER SCISSORS]
+    end
   end
 
-  # test "user can choose R" do
-  #   assert capture_io([input: "R", capture_prompt: false], fn ->
-  #     IO.write RockPaperScissors.choose()
-  #   end) == "ROCK"
-  # end
+  describe "choice_prompt/0" do
+    test "should prompt a message" do
+      assert capture_io(fn ->
+        RockPaperScissors.choice_prompt()
+      end) =~ "Choose"
+    end
 
-  # test "user can choose P" do
-  #   assert capture_io([input: "P", capture_prompt: false], fn ->
-  #     IO.write RockPaperScissors.choose()
-  #   end) == "PAPER"
-  # end
+    test "should return any string input" do
+      assert capture_io([input: "r", capture_prompt: false], fn ->
+        IO.write RockPaperScissors.choice_prompt()
+      end) == "r"
 
-  # test "user can choose S" do
-  #   assert capture_io([input: "S", capture_prompt: false], fn ->
-  #     IO.write RockPaperScissors.choose()
-  #   end) == "SCISSORS"
-  # end
+      assert capture_io([input: "boo", capture_prompt: false], fn ->
+        IO.write RockPaperScissors.choice_prompt()
+      end) == "boo"
+
+    end
+  end
+
+  describe "evaluate_choices/1 where AI wins and User loses" do
+    test "if :ai_choice ROCK and :users_choice SCISSORS, then :game_status is set to :lose" do
+      game =  %RockPaperScissors{
+        ai_choice: "ROCK",
+        users_choice: "SCISSORS",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :lose} = RockPaperScissors.evaluate_choices(game)
+    end
+
+    test "if :ai_choice PAPER and :users_choice ROCK, then :game_status is set to :lose" do
+      game =  %RockPaperScissors{
+        ai_choice: "PAPER",
+        users_choice: "ROCK",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :lose} = RockPaperScissors.evaluate_choices(game)
+    end
+
+    test "if :ai_choice SCISSORS and :users_choice PAPER, then :game_status is set to :lose" do
+      game =  %RockPaperScissors{
+        ai_choice: "SCISSORS",
+        users_choice: "PAPER",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :lose} = RockPaperScissors.evaluate_choices(game)
+    end
+  end
+
+  describe "evaluate_choices/1 where AI loses and User wins" do
+    test "if :ai_choice SCISSORS and :users_choice ROCK, then :game_status is set to :win" do
+      game =  %RockPaperScissors{
+        ai_choice: "SCISSORS",
+        users_choice: "ROCK",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :win} = RockPaperScissors.evaluate_choices(game)
+    end
+
+    test "if :ai_choice ROCK and :users_choice PAPER, then :game_status is set to :win" do
+      game =  %RockPaperScissors{
+        ai_choice: "ROCK",
+        users_choice: "PAPER",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :win} = RockPaperScissors.evaluate_choices(game)
+    end
+
+    test "if :ai_choice PAPER and :users_choice SCISSORS, then :game_status is set to :win" do
+      game =  %RockPaperScissors{
+        ai_choice: "PAPER",
+        users_choice: "SCISSORS",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :win} = RockPaperScissors.evaluate_choices(game)
+    end
+  end
+
+  describe "evaluate_choices/1 when there is a draw or invalid choice" do
+    test "if :ai_choice and :users_choice are the same, then :game_status is set to :draw" do
+      game =  %RockPaperScissors{
+        ai_choice: "PAPER",
+        users_choice: "PAPER",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :draw} = RockPaperScissors.evaluate_choices(game)
+    end
+
+    test "if :users_choice is 'invalid', then :game_status is set to :invalid_choice" do
+      game =  %RockPaperScissors{
+        ai_choice: "PAPER",
+        users_choice: "invalid",
+        game_status: nil
+      }
+      assert %RockPaperScissors{game_status: :invalid_choice} = RockPaperScissors.evaluate_choices(game)
+    end
+
+  end
+
+  describe "print_result/1" do
+    test "when :game_status is :win" do
+      game =  %RockPaperScissors{
+             ai_choice: "PAPER",
+             users_choice: "SCISSORS",
+             game_status: :win
+           }
+      assert capture_io(fn ->
+          RockPaperScissors.print_result(game)
+        end) =~ "You WIN!"
+    end
+
+    test "when :game_status is :invalid_choice" do
+      game =  %RockPaperScissors{
+             ai_choice: "PAPER",
+             users_choice: "invalid",
+             game_status: :invalid_choice
+           }
+      assert capture_io(fn ->
+          RockPaperScissors.print_result(game)
+        end) =~ "Can't do that fucker"
+    end
+
+    test "when :game_status is :lose" do
+      game =  %RockPaperScissors{
+             ai_choice: "PAPER",
+             users_choice: "ROCK",
+             game_status: :lose
+           }
+      assert capture_io(fn ->
+          RockPaperScissors.print_result(game)
+        end) =~ "You LOSE!"
+    end
+
+    test "when :game_status is :draw" do
+      game =  %RockPaperScissors{
+             ai_choice: "PAPER",
+             users_choice: "PAPER",
+             game_status: :draw
+           }
+      assert capture_io(fn ->
+          RockPaperScissors.print_result(game)
+        end) =~ "DRAW!"
+    end
+
+  end
 end
